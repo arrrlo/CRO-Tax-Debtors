a0aa1f162dd069e42d7916c9b01f192804a34043
Ivan Arar <ivan@ivan-HP-ProBook-6460b.(none)>
2012-08-02 11:53:22 +0200

    skripta za parsanje

---
+++ pd.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+import os
+from os import path
+
+current_dir = path.dirname(path.realpath(__file__))
+sys.path.append(path.join(current_dir, path.pardir))
+
+import requests
+import lxml.html as lxml_html
+from StringIO import StringIO
+
+def save_data(remote_url):
+	response = requests.get(remote_url)
+	html_parsed = lxml_html.parse(StringIO(response.content))
+
+	usersData = []
+	for table in html_parsed.iter('table'):
+		fields = []
+		if table.attrib.has_key('class') and table.attrib['class'] == 'dataTable':
+			for tr in table.iter('tr'):
+				if tr.attrib.has_key('class') and tr.attrib['class'] == 'tableHeader':
+					for td in tr.iter('td'):
+						fields.append(td.text_content().strip(' \t\n\r'))
+						# nazivi polja
+				else:
+					userData = {}
+					for i, td in enumerate(tr.iter('td')):
+						userData[fields[i]] = td.text_content().strip(' \t\n\r')
+						# podaci
+					usersData.append(userData)
+	print usersData
+
+if __name__ == "__main__":
+    save_data('http://duznici.porezna-uprava.hr/fo/svi/1.html')
